3f2eb1f4118a26a76cd50914a62a2d709f603e1c
hayzedd2 <azeezalhameen1@gmail.com>
2023-09-20 00:35:27 +0100

    second push

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,11 +1,57 @@
-import React from 'react'
+import React from "react";
+import faqImage from '../images/cwok_casual_21 1.png'
 
 const FAQ = () => {
   return (
-    <div>
-        
-    </div>
-  )
-}
+    <>
+      <section className="faq-section">
+        <div className="faq-container">
+          <div className="faq-flex">
+            <div className="faq-text">
+              <div className="faq-heading">
+                <h1>frequently asked <span>questions</span></h1>
+                <p>
+                  We got answers to the questions that you might want to ask
+                  about getlinked Hackathon 1.0
+                </p>
+              </div>
+              <div className="faq-accordion">
+                <ul>
+                  <li>
+                    Can I work on a project I started before the hackathon?{" "}
+                    <span>+</span>
+                  </li>
+                  <li>
+                  What happens if I need help during the hackathon?{" "}
+                    <span>+</span>
+                  </li>
+                  <li>
+                  What happens if I don't have an idea for a project?{" "}
+                    <span>+</span>
+                  </li>
+                  <li>
+                  Can I join a team or do I have to come with one?{" "}
+                    <span>+</span>
+                  </li>
+                  <li>
+                  What happens after the hackathon ends?{" "}
+                    <span>+</span>
+                  </li>
+                  <li>
+                  Can I work on a project I started before the hackathon?{" "}
+                    <span>+</span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="faq-img">
+                <img src={faqImage} alt="" />
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default FAQ
+export default FAQ;
